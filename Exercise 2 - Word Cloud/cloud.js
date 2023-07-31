5f3484856e807e70c8aca4d0b717d1012cde2d9c
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-07-31 10:08:38 +0200

    Find 12 most occuring words

--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,1 +1,26 @@
-console.log("Exercise 2 - Word Cloud");
+//  Select the paragraph and store it into a variable
+const paragraph = document.getElementById("myParagraph");
+
+// Get the text content inside that paragraph
+const words = paragraph.textContent;
+// Split the text into an array of words
+const wordsArray = words.match(/\b\w+\b/gi);
+//find the words count and store it into an object
+const wordsCount = {};
+wordsArray.forEach((word) => {
+  wordsCount[word] = (wordsCount[word] || 0) + 1;
+});
+
+//Sort the whole object count in descending order
+const sortedWords = Object.keys(wordsCount).sort(
+  (a, b) => wordsCount[b] - wordsCount[a]
+);
+
+//Store the 12 most occuring words into an array
+
+const most12 = sortedWords.slice(0, 12);
+console.log(most12);
+// Select the cloud div
+const wordCloud = document.getElementById("myWordCloud");
+
+//
